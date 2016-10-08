762dfab24b4ebdf5c08105788f3df442a64b2fad
Zak Golding <zak@0x7cf.com>
2016-10-07 19:25:44 -0700

    Adjusted a couple things

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
+var path = require('path');
 var app = express();
 
 var port = 3300; // port
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(express.static(__dirname + 'compiled/views/')); // static files
+app.use(express.static(path.join(__dirname, 'compiled/views/'))); // static files
 
 app.listen(port, function () {
 	// listening
